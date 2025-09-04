078debe26191c1c0c0de86675891e0deb2aec28c
hammadkhan1233 <134134490+hammadkhan1233@users.noreply.github.com>
2025-09-04 23:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
-import random
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -45,4 +45,5 @@ def handle_disconnect():
             waiting_users.remove(sid)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns port
+    socketio.run(app, host="0.0.0.0", port=port)
